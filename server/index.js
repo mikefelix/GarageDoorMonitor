8c5dcf9541b84c0c0c623757d732d3cecf37ae4b
mikefelix <michael.felix@gmail.com>
2016-08-09 20:13:54 -0600

    Add logging, fix calls

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ if (!process.argv[4]){
 var port = process.argv[2]; 
 var emailAddress = process.argv[3];
 var tesselAddress = process.argv[4];
+var authKey = process.argv[5];
 
 function reply(res, msg){
     if (typeof msg == 'number'){
@@ -25,12 +26,8 @@ function reply(res, msg){
     res.end();
 }
 
-function auth(token){
-  return token == 'bubblicious';
-}
-
 function mail(subj, body){
-    var cmd = 'echo "' + subj + '" | /usr/bin/mail -s "' + body + '" ' + emailAddress;
+    var cmd = 'echo "' + body + '" | /usr/bin/mail -s "' + subj + '" ' + emailAddress;
 
     exec(cmd, function callback(error, stdout, stderr){
         if (error) console.log("Failed to mail. " + error);
@@ -41,14 +38,17 @@ http.createServer(function(req, response) {
     var uri = url.parse(req.url).pathname; 
 
     if (uri == '/warn1'){
+        console.log("Send warning 1 at " + new Date());
         mail("Garage left open", "The garage has been open for too long. Shutting it.");
         reply(response, "OK");
     }
-    if (uri == '/warn2'){
+    else if (uri == '/warn2'){
+        console.log("Send warning 2 at " + new Date());
         mail("Garage failed to close", "Garage didn't shut when I tried. Trying again.");
         reply(response, "OK");
     }
-    if (uri == '/warn3'){
+    else if (uri == '/warn3'){
+        console.log("Send warning 3 at " + new Date());
         mail("Garage is stuck open!", "Garage is open and I cannot shut it! I tried twice.");
         reply(response, "OK");
     }
@@ -56,38 +56,59 @@ http.createServer(function(req, response) {
         console.log("Tessel is alive at " + new Date());
         reply(response, "Yay!");
     }
+    else if (uri == '/forceopened'){ // call from tessel
+        console.log('Tessel reports forced open state at ' + new Date());
+        reply(response, "forcedopen alert received");
+    }
     else if (uri == '/opened'){ // call from tessel
-        console.log('opened');
+        console.log('Tessel reports opened state at ' + new Date());
         reply(response, "open alert received");
     }
     else if (uri == '/closed'){ // call from tessel
-        console.log('closed');
+        console.log('Tessel reports closed state at ' + new Date());
         reply(response, "closed alert received");
     }
+    else if (uri == '/toggle'){ // call from user
+        if (new RegExp('auth=' + authKey).test(req.url)){
+            console.log('Toggle command received at ' + new Date());
+            callTessel('toggle', function(msg){
+                reply(response, msg);
+            });
+        }
+        else {
+            console.log('401 on toggle command at ' + new Date());
+            reply(response, 401);
+        }
+    }
     else if (uri == '/close'){ // call from user
-        if (/auth=gungeon/.test(req.url)){
+        if (new RegExp('auth=' + authKey).test(req.url)){
+            console.log('Close command received at ' + new Date());
             callTessel('close', function(msg){
                 reply(response, msg);
             });
         }
         else {
+            console.log('401 on close command at ' + new Date());
             reply(response, 401);
         }
     }
     else if (uri == '/open'){ // call from user
-        if (/auth=gungeon/.test(req.url)){
+        if (new RegExp('auth=' + authKey).test(req.url)){
             if (/force=true/.test(req.url)){
+                console.log('Force open command received at ' + new Date());
                 callTessel('forceopen', function(msg){
                     reply(response, msg);
                 });
             }
             else {
+                console.log('Open command received at ' + new Date());
                 callTessel('open', function(msg){
                     reply(response, msg);
                 });
             }
         }
         else {
+            console.log('401 on open at ' + new Date());
             reply(response, 401);
         }
     }
@@ -96,7 +117,7 @@ http.createServer(function(req, response) {
             reply(response, msg);
         });
     }
-    else if (uri == '/home'){ // call from tessel
+    else if (uri == '/home'){ 
         console.log("Nest home at " + new Date());
         exec('/home/felix/bin/snapshot.sh', function callback(error, stdout, stderr){
             if (error) console.log("Failed to save snapshot. " + error);
